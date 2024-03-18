e4a76fe7b743a47ea3fba26c0787c8efac00764a
Hsin Kai <hsinkai2000@gmail.com>
2024-03-18 20:28:51 +0800

    Google popup login complete
    successfully completed a google login into firebase authentication

--- src/lovelens/src/Components/Web/Login.jsx
+++ src/lovelens/src/Components/Web/Login.jsx
@@ -1,7 +1,52 @@
 import '../Styles/Login.css'
 import weddingpic from '../../images/weddingpic.png'
+import { useState } from "react";
+import {
+    signInAuthUserWithEmailAndPassword,
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+} from "../../utils/firebase.utils";
+
+const defaultValues = {
+    email: "",
+    password: "",
+};
 
 export default function Login() {
+    const [fields, setFields] = useState(defaultValues);
+    const { email, password } = fields;
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { user } = await signInAuthUserWithEmailAndPassword(
+                email,
+                password
+            );
+        } catch (error) {
+            switch (error.code) {
+                case "auth/wrong-password":
+                    alert("Incorrect password");
+                    break;
+                case "auth/user-not-found":
+                    alert("No user with this email address was found");
+                    break;
+                default:
+                    console.log(error);
+            }
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFields({ ...fields, [name]: value });
+    };
+
+    const signInWithGoogle = async () => {
+        await signInWithGooglePopup();
+    };
+
     return(
         <div className='loginPageContainer'>
             <div className="contentContainer">
@@ -13,22 +58,23 @@ export default function Login() {
                     <div className='loveLens'><p className='prodName'>LOVE LENS</p></div>
                     <div className='loginSectionContainer '>
                         <h1>LOGIN/SIGN UP</h1>
-                        <form className='loginForm'>
+                        <form className='loginForm' onSubmit={submitHandler}>
                             <div className='emailContainer'>
                                 <label>Email</label>
-                                <input type='email' required></input>
+                                <input label="Email" name='email' type='email' value={email} onChange={handleChange} required></input>
                             </div>
                             <div className='passwordContainer'>
                                 <label>Password</label>
-                                <input type='password' required></input>
+                                <input label="Password" name='password' value={password} onChange={handleChange} required></input>
                             </div>
-                            <button className='loginButton'>LOGIN/SIGNUP</button>
+                            <button className='loginButton' type="submit">LOGIN/SIGNUP</button>
+
                             <div className='otherLogins'>
                                 <div className='hrContainer'><hr></hr></div>
                                 <p>login with others</p>
                                 <div className='hrContainer'><hr></hr></div>
                             </div>
-                            <button className='googleLoginButton'>LOGIN WITH GOOGLE</button>
+                            <button className='googleLoginButton' onClick={signInWithGoogle}>LOGIN WITH GOOGLE</button>
                         </form>
                     </div>
                 </div>
