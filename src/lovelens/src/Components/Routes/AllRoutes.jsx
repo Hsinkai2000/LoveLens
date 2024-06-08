ce93f63b3b01d3e47ab3527ffa24730e2fdccce7
Ning Hui <ninghuiker@gmail.com>
2024-06-08 20:51:54 +0800

    Updated the Routing

--- src/lovelens/src/Components/Routes/AllRoutes.jsx
+++ src/lovelens/src/Components/Routes/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { NavigationPaths } from './NavigationPaths';
 import LandingPage from '../LandingPage';
+import Login from '../Web/Login';
 
 export default function AllRoutes() {
     return (
@@ -8,6 +9,10 @@ export default function AllRoutes() {
             <Route
                 path={NavigationPaths.landingPath}
                 element={<LandingPage />}
+            ></Route>
+            <Route
+                path={NavigationPaths.loginPath}
+                element={<Login />}
             ></Route>
         </Routes>
     );
